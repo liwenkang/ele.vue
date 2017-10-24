b6f8d2489565e3c22ad5d4f461b687c0aed7a445
liwenkang <changzhiliwenkang@outlook.com>
2017-10-24 17:19:56 +0800

    完成header的弹出窗口
    就我这个效率,做完估计地球都要被毁灭了

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,30 +17,80 @@
           <div class="text">{{seller.supports[0].description}}</div>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
     </div>
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
+    <div v-show="detailShow" class="detail" transition="change">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :size="48" :score="seller.score"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">优惠信息</div>
+            <div class="line"></div>
+          </div>
+          <ul v-if="seller.supports" class="supports">
+            <li class="support-item" v-for="(item, index) in seller.supports">
+              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+              <span class="text">{{seller.supports[index].description}}</span>
+            </li>
+          </ul>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="detail-close" @click="hideDetail">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from '../star/star.vue'
+
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
+      },
+      hideDetail() {
+        this.detailShow = false
+      }
+    },
     created() {
       this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+    },
+    components: {
+      star
     }
   }
 </script>
@@ -51,8 +101,8 @@
   // 这是为了引入小图标,我咋就搞不定呢
   @font-face {
     font-family: 'sell-icon';
-    src:  url('../../common/fonts/sell-icon.eot?exau1o');
-    src:  url('../../common/fonts/sell-icon.eot?exau1o#iefix') format('embedded-opentype'),
+    src: url('../../common/fonts/sell-icon.eot?exau1o');
+    src: url('../../common/fonts/sell-icon.eot?exau1o#iefix') format('embedded-opentype'),
       url('../../common/fonts/sell-icon.ttf?exau1o') format('truetype'),
       url('../../common/fonts/sell-icon.woff?exau1o') format('woff'),
       url('../../common/fonts/sell-icon.svg?exau1o#sell-icon') format('svg');
@@ -78,38 +128,47 @@
   .icon-add_circle:before {
     content: "\e900";
   }
+
   .icon-arrow_lift:before {
     content: "\e901";
   }
+
   .icon-check_circle:before {
     content: "\e902";
   }
+
   .icon-close:before {
     content: "\e903";
   }
+
   .icon-favorite:before {
     content: "\e904";
   }
+
   .icon-keyboard_arrow_right:before {
     content: "\e905";
   }
+
   .icon-remove_circle_outline:before {
     content: "\e906";
   }
+
   .icon-shopping_cart:before {
     content: "\e907";
   }
+
   .icon-thumb_down:before {
     content: "\e908";
   }
+
   .icon-thumb_up:before {
     content: "\e909";
   }
 
-
   .header
     position relative
-    background-color rgba(7,17,27,0.5)
+    overflow hidden
+    background-color rgba(7, 17, 27, 0.5)
     color #fff
     .content-wrapper
       position relative
@@ -175,7 +234,7 @@
         height 24px
         line-height 24px
         border-radius 14px
-        background-color: rgba(0,0,0,0.2)
+        background-color: rgba(0, 0, 0, 0.2)
         text-align center
         .count
           vertical-align top
@@ -192,7 +251,7 @@
       white-space: nowrap
       overflow: hidden
       text-overflow: ellipsis
-      background-color: rgba(7,17,27,0.2)
+      background-color: rgba(7, 17, 27, 0.2)
       .bulletin-title
         display inline-block
         vertical-align top
@@ -219,4 +278,87 @@
       height 100%
       z-index -1
       filter blur(10px)
+    .detail
+      transition all 10s
+      background: rgba(7,17,27,0.8)
+      position fixed
+      top 0
+      left 0
+      z-index 100
+      width 100%
+      height 100%
+      overflow auto
+      .detail-wrapper
+        width 100%
+        min-height 100% //最小高度,撑满屏幕,否则当内容很少时,关闭按钮无法定到下面的位置
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px //这个一定要padding-bottom,要在这里放关闭按钮
+          .name
+            line-height 16px
+            text-align center
+            font-weight 700
+            font-size 16px
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0px
+            text-align center
+          .title
+            display flex
+            width 80%
+            margin 28px auto 24px
+            .line
+              flex 1
+              position relative
+              top -6px
+              border-bottom 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding 0 12px
+              font-weight 700
+              font-size 14px
+          .supports
+            width 80%
+            margin 0 auto
+            .support-item
+              padding 0 12px
+              margin-bottom 12px
+              font-size 0
+              &:last-child
+                margin-bottom 0
+              .icon
+                display inline-block
+                width 16px
+                height 16px
+                vertical-align top
+                margin-right 6px
+                background-size 16px 16px
+                background-repeat no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                line-height 14px
+                font-size 12px
+          .bulletin
+            width 80%
+            margin 0 auto
+            .content
+              padding 0 12px
+              line-height 24px
+              font-size 12px
+
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto 0 auto
+        clear both
+        font-size 32px
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const LENGTH = 5
+  const clsOn = 'on'
+  const clsHalf = 'half'
+  const clsOff = 'off'
+
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size
+      },
+      itemClasses() {
+        let result = []
+        let score = Math.floor(this.score * 2) / 2  // 向下取0.5
+        let hasDecimal = (score % 1 !== 0) // 有半个星
+        let integer = Math.floor(score) // 有几个全星
+        for (let i = 0; i < integer; i++) {
+          result.push(clsOn)
+        }
+        if (hasDecimal) {
+          result.push(clsHalf)
+        }
+        while (result.length < LENGTH) {
+          result.push(clsOff)
+        }
+        return result
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/_mixins.scss"
+
+  .star
+    font-size 0
+    .star-item
+      display inline-block
+      background-repeat: no-repeat
+    &.star-48
+      .star-item
+        width 20px
+        height 20px
+        margin-right 22px
+        background-size: 20px 20px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('star48_on')
+        &.half
+          bg-image('star48_half')
+        &.off
+          bg-image('star48_off')
+    &.star-36
+      .star-item
+        width 15px
+        height 15px
+        margin-right 12px
+        background-size: 15px 15px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('star36_on')
+        &.half
+          bg-image('star36_half')
+        &.off
+          bg-image('star36_off')
+    &.star-24
+      .star-item
+        width 10px
+        height 10px
+        margin-right 3px
+        background-size: 10px 10px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('star24_on')
+        &.half
+          bg-image('star24_half')
+        &.off
+          bg-image('star24_off')
+</style>
